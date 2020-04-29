46e8d8a7b78c8b5a24a8ac23f461bd40272755b4
Matt Young <mmyoung979@gmail.com>
2020-04-29 04:52:27 -0400

    Styling

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't0lb&)s+(e^7ib2ei^e)4@bk+q#!=tb!x#tyr1uai+^rn)qb$&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'mattmyoung.com']
 
