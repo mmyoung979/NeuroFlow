cd0daf739fcbe1f5d58bc773008f61a08d2e6aa8
Matt Young <mmyoung979@gmail.com>
2020-04-29 01:51:18 -0400

    Updated .gitignore

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/matthewyoung/Documents/Programming/GitHub/neuroflow/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
