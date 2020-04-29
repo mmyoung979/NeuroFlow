5288626ef8ff08ee74ceeb1e7d6213964c9dec48
Matt Young <mmyoung979@gmail.com>
2020-04-29 03:25:58 -0400

    Production Settings

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't0lb&)s+(e^7ib2ei^e)4@bk+q#!=tb!x#tyr1uai+^rn)qb$&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
